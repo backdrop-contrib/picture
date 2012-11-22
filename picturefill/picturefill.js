7ab883556bc79a8556cef0d240cff27f2200499b
luksak <luksak@598412.no-reply.drupal.org>
2012-11-22 14:00:44 +0100

    Issue #1841062 by Lukas von Blarer: Check if the span is a picture.

--- picturefill/picturefill.js
+++ picturefill/picturefill.js
@@ -19,96 +19,98 @@
 
     // Loop the pictures.
     for (var i = 0, il = ps.length; i < il; i++ ) {
-      var sources = ps[i].getElementsByTagName('span');
-      var picImg = null;
-      var matches = [];
+      if (ps[i].getAttribute('data-picture') !== null) {
+        var sources = ps[i].getElementsByTagName('span');
+        var picImg = null;
+        var matches = [];
 
-      // See which sources match.
-      for (var j = 0, jl = sources.length; j < jl; j++ ) {
-        var media = sources[j].getAttribute('data-media');
+        // See which sources match.
+        for (var j = 0, jl = sources.length; j < jl; j++ ) {
+          var media = sources[j].getAttribute('data-media');
 
-        // adds -webkit- prefix in javascript instead of HTML, for simplicity
-        if (media && media.indexOf("device-pixel-ratio") > -1 ) {
-          var webkitmedia = media.replace("device-pixel-ratio", "-webkit-device-pixel-ratio");
-          var mozmedia = media.replace("device-pixel-ratio", "-moz-device-pixel-ratio");
-          var omedia = media.replace("device-pixel-ratio", "-o-device-pixel-ratio");
-          media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
-        }
-        if (media && media.indexOf("min-device-pixel-ratio") > -1 ) {
-          var webkitmedia = media.replace("min-device-pixel-ratio", "-webkit-min-device-pixel-ratio");
-          var mozmedia = media.replace("min-device-pixel-ratio", "min--moz-device-pixel-ratio");
-          var omedia = media.replace("min-device-pixel-ratio", "-o-min-device-pixel-ratio");
-          media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
-        }
-        if (media && media.indexOf("max-device-pixel-ratio") > -1 ) {
-          var webkitmedia = media.replace("max-device-pixel-ratio", "-webkit-max-device-pixel-ratio");
-          var mozmedia = media.replace("max-device-pixel-ratio", "max--moz-device-pixel-ratio");
-          var omedia = media.replace("max-device-pixel-ratio", "-o-max-device-pixel-ratio");
-          media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
-        }
-
-        // If there's no media specified or the media query matches, add it.
-        if (!media || (w.matchMedia && w.matchMedia(media).matches)) {
-          matches.push(sources[j]);
-        }
-      }
-
-      if (matches.length) {
-        // Grab the most appropriate (last) match.
-        var match = matches.pop();
-        var srcset = match.getAttribute('data-srcset');
-
-        // Find any existing img element in the picture element.
-        picImg = ps[i].getElementsByTagName('img')[0];
-
-        // Add a new img element if one doesn't exists.
-        if (!picImg) {
-          picImg = w.document.createElement('img');
-          picImg.alt = ps[i].getAttribute('data-alt');
-          ps[i].appendChild(picImg);
-        }
-
-        // Source element uses a srcset.
-        if (srcset) {
-          var screenRes = w.devicePixelRatio || 1;
-          // Split comma-separated `srcset` sources into an array.
-          sources = srcset.split(', ');
-
-          // Loop through each source/resolution in srcset.
-          for (var res = sources.length, r = res - 1; r >= 0; r-- ) {
-            // Remove any leading whitespace, then split on spaces.
-            var source = sources[ r ].replace(/^\s*/, '').replace(/\s*$/, '').split(' ');
-            // Parse out the resolution for each source in `srcset`.
-            var resMatch = parseFloat(source[1], 10);
-
-            if (screenRes >= resMatch) {
-              if (picImg.getAttribute('src') !== source[0]) {
-                var newImg = document.createElement('img');
-
-                newImg.src = source[0];
-                // When the image is loaded, set a width equal to that of the
-                // original’s intrinsic width divided by the screen resolution.
-                newImg.onload = function() {
-                  // Clone the original image into memory so the width is
-                  // unaffected by page styles.
-                  var w = this.cloneNode(true).width;
-                  if (w > 0) {
-                    this.width = (w / resMatch);
-                  }
-                };
-                // Copy width and height from the source tag to the img element.
-                _copyAttributes(match, newImg);
-                picImg.parentNode.replaceChild(newImg, picImg);
-              }
-              // We’ve matched, so bail out of the loop here.
-              break;
-            }
+          // adds -webkit- prefix in javascript instead of HTML, for simplicity
+          if (media && media.indexOf("device-pixel-ratio") > -1 ) {
+            var webkitmedia = media.replace("device-pixel-ratio", "-webkit-device-pixel-ratio");
+            var mozmedia = media.replace("device-pixel-ratio", "-moz-device-pixel-ratio");
+            var omedia = media.replace("device-pixel-ratio", "-o-device-pixel-ratio");
+            media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
           }
-        } else {
-          // No srcset used, so just use the 'src' value.
-          picImg.src = match.getAttribute('data-src');
-          // Copy width and height from the source tag to the img element.
-          _copyAttributes(match, picImg);
+          if (media && media.indexOf("min-device-pixel-ratio") > -1 ) {
+            var webkitmedia = media.replace("min-device-pixel-ratio", "-webkit-min-device-pixel-ratio");
+            var mozmedia = media.replace("min-device-pixel-ratio", "min--moz-device-pixel-ratio");
+            var omedia = media.replace("min-device-pixel-ratio", "-o-min-device-pixel-ratio");
+            media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
+          }
+          if (media && media.indexOf("max-device-pixel-ratio") > -1 ) {
+            var webkitmedia = media.replace("max-device-pixel-ratio", "-webkit-max-device-pixel-ratio");
+            var mozmedia = media.replace("max-device-pixel-ratio", "max--moz-device-pixel-ratio");
+            var omedia = media.replace("max-device-pixel-ratio", "-o-max-device-pixel-ratio");
+            media = webkitmedia +","+ mozmedia +","+ omedia +","+ media;
+          }
+
+          // If there's no media specified or the media query matches, add it.
+          if (!media || (w.matchMedia && w.matchMedia(media).matches)) {
+            matches.push(sources[j]);
+          }
+        }
+
+        if (matches.length) {
+          // Grab the most appropriate (last) match.
+          var match = matches.pop();
+          var srcset = match.getAttribute('data-srcset');
+
+          // Find any existing img element in the picture element.
+          picImg = ps[i].getElementsByTagName('img')[0];
+
+          // Add a new img element if one doesn't exists.
+          if (!picImg) {
+            picImg = w.document.createElement('img');
+            picImg.alt = ps[i].getAttribute('data-alt');
+            ps[i].appendChild(picImg);
+          }
+
+          // Source element uses a srcset.
+          if (srcset) {
+            var screenRes = w.devicePixelRatio || 1;
+            // Split comma-separated `srcset` sources into an array.
+            sources = srcset.split(', ');
+
+            // Loop through each source/resolution in srcset.
+            for (var res = sources.length, r = res - 1; r >= 0; r-- ) {
+              // Remove any leading whitespace, then split on spaces.
+              var source = sources[ r ].replace(/^\s*/, '').replace(/\s*$/, '').split(' ');
+              // Parse out the resolution for each source in `srcset`.
+              var resMatch = parseFloat(source[1], 10);
+
+              if (screenRes >= resMatch) {
+                if (picImg.getAttribute('src') !== source[0]) {
+                  var newImg = document.createElement('img');
+
+                  newImg.src = source[0];
+                  // When the image is loaded, set a width equal to that of the
+                  // original’s intrinsic width divided by the screen resolution.
+                  newImg.onload = function() {
+                    // Clone the original image into memory so the width is
+                    // unaffected by page styles.
+                    var w = this.cloneNode(true).width;
+                    if (w > 0) {
+                      this.width = (w / resMatch);
+                    }
+                  };
+                  // Copy width and height from the source tag to the img element.
+                  _copyAttributes(match, newImg);
+                  picImg.parentNode.replaceChild(newImg, picImg);
+                }
+                // We’ve matched, so bail out of the loop here.
+                break;
+              }
+            }
+          } else {
+            // No srcset used, so just use the 'src' value.
+            picImg.src = match.getAttribute('data-src');
+            // Copy width and height from the source tag to the img element.
+            _copyAttributes(match, picImg);
+          }
         }
       }
     }
